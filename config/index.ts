61c77a2d5f54f6c5bdfe96c1788cc4d84cf251b9
Kili S <79143155+klinshy@users.noreply.github.com>
2024-08-02 16:45:07 +0200

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,10 +4,10 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
-  privacy_policy: '',
+  title: 'kOS AI',
+  description: 'The AI that can help you with your daily tasks',
+  copyright: 'NewIT.works',
+  privacy_policy: 'tba',
   default_language: 'zh-Hans',
 }
 
